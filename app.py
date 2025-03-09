9f851f891d7a199c59a292cdf42dca629f851726
ritik <ritik.nimoy@gemseducation.com>
2025-03-09 11:09:40 +0530

    added prints for requests

--- app.py
+++ app.py
@@ -5,8 +5,17 @@ app = Flask(__name__)
 @app.route('/webhook', methods=['POST'])
 def webhook():
     validation_token = request.args.get('validationtoken')
-    print(validation_token)
-    print(request.args)
+    print("Query Parameters:", request.args)
+
+    # Print raw request data
+    print("Raw Data:", request.data)
+
+    # Print JSON data if available
+    if request.is_json:
+        print("JSON Data:", request.json)
+
+    # Print form data if applicable
+    print("Form Data:", request.form)
     if validation_token:
         print("returning v",validation_token)
         return validation_token, 200
